956f7519a406fe4b7c0228db41436a76c4432ec3
charles <celliwig@nym.hush.com>
2020-07-23 00:56:47 +0000

    Meru device Makefile updated to build the components (FDT/Kernel/RootFS) implementing squashfs.

--- image/meru.mk
+++ image/meru.mk
@@ -1,33 +1,20 @@
 DEVICE_VARS += ROOTFS_SIZE
 
-define Build/fullimage
-	rm -f ${IMAGE_ROOTFS}.gz
-	gzip -9nk ${IMAGE_ROOTFS}
-	export kernel_load_addr=0x`${NM} $(KDIR)/cuImage.meru-ap320.elf | grep ' _start' | cut -d' ' -f1`; \
-	export kernel_entry_addr=`${OBJDUMP} -f $(KDIR)/cuImage.meru-ap320.elf | grep '^start address ' | cut -d' ' -f3`; \
-	mkimage -A powerpc -O linux -C gzip -T multi \
-		-a $$kernel_load_addr -e $$kernel_entry_addr \
-		-n 'Meru Networks AP320 $(VERSION_DIST) Linux-$(LINUX_VERSION)' \
-		-d $(KDIR)/cuImage.meru-ap320.gz:${IMAGE_ROOTFS}.gz $@
-endef
-
 define Device/ap320
 	DEVICE_VENDOR := Meru Networks
 	DEVICE_MODEL := AP320
 	BOARDNAME := AP320
 	DEVICE_PACKAGES += wpad-basic
-	ROOTFS_SIZE := 5040k
-	IMAGE_SIZE := 5040k
-#	KERNEL_NAME := cuImage.meru-ap320.elf
-	KERNEL := kernel-bin | gzip
+	UIMAGE_SIZE := 4096k
+	ROOTFS_SIZE := 11008k
+	KERNEL := kernel-bin
 	DEVICE_DTS := meru-ap320
-#	MTDPARTS := spi0.0:256k(u-boot)ro,256k(u-boot-env)ro,13440k(rootfs),2240k(kernel),64k(mac),128k(art)ro,15680k@0x80000(firmware)
-#	IMAGES := kernel.bin rootfs.bin sysupgrade.bin
-	IMAGES := fullimage.bin
-#	IMAGE/kernel.bin := append-kernel | gzip | uImage gzip
-#	IMAGE/rootfs.bin := append-rootfs | gzip
-#	IMAGE/fullimage.bin := fullimage | check-size
-	IMAGE/fullimage.bin := fullimage
+	FILESYSTEMS := squashfs
+	IMAGES := kernel.bin rootfs.bin fdt.bin
+	IMAGE/kernel.bin := append-kernel | gzip | uImage gzip | pad-to $$$$(UIMAGE_SIZE)
+#	IMAGE/rootfs.bin := append-rootfs | pad-rootfs | pad-to $$$$(ROOTFS_SIZE)
+	IMAGE/rootfs.bin := append-rootfs | pad-rootfs
+	IMAGE/fdt.bin := append-dtb | pad-to 128k
 #	IMAGE/sysupgrade.bin := append-rootfs | pad-rootfs | pad-to $$$$(ROOTFS_SIZE) | append-kernel | check-size $$$$(IMAGE_SIZE)
 endef
 TARGET_DEVICES += ap320
